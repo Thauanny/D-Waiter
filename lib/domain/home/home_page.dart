438bfa344116f50c7c3858798335d9a6492699dd
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2022-01-09 22:26:17 -0300

    Reformulação da Home para visão de tabs

--- lib/domain/home/home_page.dart
+++ lib/domain/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:d_waiter/controllers/food_controller.dart';
 import 'package:d_waiter/domain/home/components/text_button_underlined.dart';
 import 'package:d_waiter/domain/views/see_more_page.dart';
+import 'package:d_waiter/domain/welcome_page.dart';
 import 'package:d_waiter/shared/colors.dart';
 import 'package:d_waiter/shared/components/food_card.dart';
 import 'package:flutter/material.dart';
@@ -24,134 +25,172 @@ class HomePageState extends State {
     double _navSpaceW = MediaQuery.of(context).size.width / 2.05;
     double _navSpaceH = MediaQuery.of(context).size.height / 8;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: primaryWhite,
-      body: ListView(children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: MediaQuery.of(context).size.width / 50),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: SvgPicture.asset(
-                          'assets/icons/logout.svg',
-                          semanticsLabel: 'logout',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: MediaQuery.of(context).size.height - 400,
+          elevation: 0,
+          backgroundColor: primaryWhite,
+          title: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        _iconLogOut(),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset(
+                            'assets/icons/help.svg',
+                            semanticsLabel: 'help',
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: SvgPicture.asset(
-                          'assets/icons/help.svg',
-                          semanticsLabel: 'help',
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset(
+                            'assets/icons/notes.svg',
+                            semanticsLabel: 'notes',
+                          ),
                         ),
-                      ),
-                      SizedBox(height: _navSpaceH, width: _navSpaceW),
-                      IconButton(
-                        onPressed: () {},
-                        icon: SvgPicture.asset(
-                          'assets/icons/notes.svg',
-                          semanticsLabel: 'notes',
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset(
+                            'assets/icons/shopping_cart.svg',
+                            semanticsLabel: 'shopping_cart',
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: SvgPicture.asset(
-                          'assets/icons/shopping_cart.svg',
-                          semanticsLabel: 'shopping_cart',
-                        ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: MediaQuery.of(context).size.width / 8,
-                      right: MediaQuery.of(context).size.width / 8,
-                      top: MediaQuery.of(context).size.height / 20,
-                      bottom: MediaQuery.of(context).size.height / 25),
-                  child: Column(
-                    children: [
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Cardápio",
-                          textScaleFactor: 1.5,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 20,
-                      ),
-                      _searchBar(),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 10,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: const [
-                            TextButtonUnderlined(
-                              text: '    Foods    ',
-                              fontSize: 18,
-                              interactable: true,
-                            ),
-                            TextButtonUnderlined(
-                              text: '    Drinks    ',
-                              fontSize: 18,
-                              interactable: false,
-                            ),
-                            TextButtonUnderlined(
-                              text: '    Snacks    ',
-                              fontSize: 18,
-                              interactable: false,
-                            ),
-                            TextButtonUnderlined(
-                              text: '    Sauces    ',
-                              fontSize: 18,
-                              interactable: false,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Cardápio",
+                  textScaleFactor: 1.5,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
                 ),
-                _foodList(context),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20.0, bottom: 40),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: InkWell(
-                      onTap: () {
-                        _foodController.foodList.isEmpty
-                            ? () {}
-                            : Get.to(() => const SeeMorePage());
-                      },
-                      child: Text(
-                        'See More',
-                        style: TextStyle(
-                            color: _foodController.foodList.isEmpty
-                                ? Colors.grey
-                                : primaryOrange,
-                            fontSize: 20),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 100,
+                child: _searchBar(),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const TabBar(
+                  automaticIndicatorColorAdjustment: true,
+                  labelColor: primaryOrange,
+                  isScrollable: true,
+                  indicatorColor: primaryOrange,
+                  labelStyle: TextStyle(color: primaryOrange),
+                  tabs: [
+                    Tab(
+                      child: TextButtonUnderlined(
+                        text: '   Foods    ',
+                        fontSize: 18,
+                        interactable: false,
                       ),
                     ),
-                  ),
-                ),
-              ],
-            ),
+                    Tab(
+                      child: TextButtonUnderlined(
+                        text: '   Drinks    ',
+                        fontSize: 18,
+                        interactable: false,
+                      ),
+                    ),
+                    Tab(
+                      child: TextButtonUnderlined(
+                        text: '    Snacks  ',
+                        fontSize: 18,
+                        interactable: false,
+                      ),
+                    ),
+                    Tab(
+                      child: TextButtonUnderlined(
+                        text: '    Sauces  ',
+                        fontSize: 18,
+                        interactable: false,
+                      ),
+                    ),
+                  ]),
+            ],
+          ),
+        ),
+        resizeToAvoidBottomInset: false,
+        backgroundColor: primaryWhite,
+        body: TabBarView(
+          children: [
+            _viewTab1(context),
+            _viewTab1(context),
+            _viewTab1(context),
+            _viewTab1(context)
           ],
         ),
-      ]),
+      ),
     );
   }
 }
+
+Widget _viewTab1(BuildContext context) => ListView(children: [
+      const SizedBox(
+        height: 70,
+      ),
+      _foodList(context),
+      Padding(
+        padding: const EdgeInsets.only(right: 20.0, bottom: 40),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: InkWell(
+            onTap: () {
+              _foodController.foodList.isEmpty
+                  ? () {}
+                  : Get.to(() => const SeeMorePage());
+            },
+            child: Obx(
+              () => Text(
+                'See More',
+                style: TextStyle(
+                    color: _foodController.foodList.isEmpty
+                        ? Colors.grey
+                        : primaryOrange,
+                    fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ]);
+
+Widget _iconLogOut() => IconButton(
+      onPressed: () {
+        //TODO: clean all datas
+        Get.off(() => const WelcomePage());
+      },
+      icon: SvgPicture.asset(
+        'assets/icons/logout.svg',
+        semanticsLabel: 'logout',
+      ),
+    );
 
 Widget _foodList(BuildContext context) {
   return Obx(
